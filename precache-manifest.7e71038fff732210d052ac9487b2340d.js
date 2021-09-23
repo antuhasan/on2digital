289f2b67e2392a158fb7b76bb69d7290cdceebad
antuhasan <55555169+antuhasan@users.noreply.github.com>
2021-09-23 23:40:59 +0600

    Add files via upload

---
+++ precache-manifest.7e71038fff732210d052ac9487b2340d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199cba8d83232f7c450b754c994e639e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac11860f5a0378e1152d",
+    "url": "/static/css/2.52a111dd.chunk.css"
+  },
+  {
+    "revision": "0478ba0ee98f0d42f4ad",
+    "url": "/static/css/main.6379484e.chunk.css"
+  },
+  {
+    "revision": "ac11860f5a0378e1152d",
+    "url": "/static/js/2.5a943d4b.chunk.js"
+  },
+  {
+    "revision": "0478ba0ee98f0d42f4ad",
+    "url": "/static/js/main.597a2e0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
